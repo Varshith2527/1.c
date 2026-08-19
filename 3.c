49bee2503673dd6cec049de246785b75e2311209
MALLISETTI SAI VARSHITH <msaivarsh25@gmail.com>
2026-08-19 21:57:09 +0530

    Add income tax calculation logic in 3.c
    Implement a simple income tax calculator based on income brackets.

--- 3.c
+++ 3.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    double income, taxable_income, tax = 0.0;
+
+    printf("Enter the income: ");
+    scanf("%lf", &income);
+
+    if (income <= 150000) {
+        printf("NO TAX\n");
+        tax = 0.0;
+    } 
+    else {
+        taxable_income = income - 150000;
+
+        if (income <= 300000) {
+            tax = taxable_income * 0.10;
+        } 
+        else if (income <= 500000) {
+            tax = taxable_income * 0.20;
+        } 
+        else {
+            tax = taxable_income * 0.30;
+        }
+    }
+
+    printf("TAX = %.2lf\n", tax);
+
+    return 0;
+}
