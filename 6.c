ca9d42c357c89520bcf17815661c83498588cc18
MALLISETTI SAI VARSHITH <msaivarsh25@gmail.com>
2026-08-27 21:48:58 +0530

    Add temperature conversion from Fahrenheit to Celsius

--- 6.c
+++ 6.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main()
+{
+    float fahrenheit, celsius;
+    printf("\n Enter the temperature in fahrenheit: ");
+    scanf("%f", &fahrenheit) ;
+    celsius = (0.56) * (fahrenheit - 32);
+    printf("\n Temperature in degrees celsius = %f", celsius);
+    return 0;
+}
